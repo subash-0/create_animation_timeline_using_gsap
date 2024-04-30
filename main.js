bfbbe989a99e5bda0f40d079a29caed4161c55c2
subash-0 <subash67335@gmail.com>
2024-04-30 09:45:59 +0545

    enviroment like tree and other stuffs are added

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
-import { GLTFLoader, SkeletonUtils,OrbitControls } from 'three/examples/jsm/Addons.js';
+import { GLTFLoader, SkeletonUtils,OrbitControls, GlitchPass } from 'three/examples/jsm/Addons.js';
 import gsap from 'gsap';
     const renderer = new THREE.WebGLRenderer({antialias:true});
     document.body.appendChild(renderer.domElement);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0XA3A3A3A3);
+    renderer.setClearColor();
 
 
 const camera = new THREE.PerspectiveCamera(
@@ -17,10 +17,11 @@ camera.position.set(10,0,20);
 const scene = new THREE.Scene();
 // const orbit = new OrbitControls(camera, renderer.domElement)
 // orbit.update();
-const ambientLight = new THREE.AmbientLight(0xffffff,2);
-const directionLight = new THREE.DirectionalLight(0xffffff,3);
+const ambientLight = new THREE.AmbientLight(0xffffff,0.8);
+const directionLight = new THREE.DirectionalLight(0xffffff,2);
 scene.add(directionLight)
 scene.add(ambientLight)
+document.body.style.cursor = 'pointer'
 // const boxMesh = new THREE.Mesh(
 //     new THREE.BoxGeometry(1,1,1),
 //     new THREE.MeshBasicMaterial({color:'green'})
@@ -31,31 +32,23 @@ scene.add(ambientLight)
 
 const phoenixLoader = new GLTFLoader();
 const dovLoader = new GLTFLoader();
+const bgLoader = new GLTFLoader();
 let mixer1;
 let mixer2;
 let mixer3;
 let mixer4;
-dovLoader.load("./assests/dove_bird_rigged/scene.gltf",(gltf)=>{
-    const model = gltf.scene;
-    scene.add(model);
-    model.scale.set(0.5,0.5,0.5);
-    model.position.set(-15,-90,0);
-    model.rotation.y = Math.PI/2
-    mixer4 = new THREE.AnimationMixer(model);
-    const  clips = gltf.animations;
-    const clip = THREE.AnimationClip.findByName(clips,"Take 001");
-    const action = mixer4.clipAction(clip);
-    action.play();
-    action.startAt(0.40);
-    window.addEventListener( "mouseover" , cameraAnimation);
 
-});
-
-phoenixLoader.load("./assests/phoenix_bird/scene.gltf",(gltf)=>{
+bgLoader.load("./assests/realistic_jungle_tree_avatar/scene.gltf",(gltf)=>{
     const model = gltf.scene;
-    model.scale.set(0.008,0.008,0.008);
+    // scene.add(model)
+})
+
+phoenixLoader.load("./assests/white_eagle_animation_fast_fly/scene.gltf",(gltf)=>{
+    const model = gltf.scene;
+    model.scale.set(0.08,0.08,0.08);
     const model22 = SkeletonUtils.clone(model)
     const model32 = SkeletonUtils.clone(model)
+    
     scene.add(model)
     scene.add(model22)
     scene.add(model32);
@@ -65,8 +58,8 @@ phoenixLoader.load("./assests/phoenix_bird/scene.gltf",(gltf)=>{
     mixer2 = new THREE.AnimationMixer(model22);
     mixer3 = new THREE.AnimationMixer(model32);
     const clips = gltf.animations;
-
-    const clip = THREE.AnimationClip.findByName(clips,"Take 001");
+    console.log(clips)
+    const clip = THREE.AnimationClip.findByName(clips,"CINEMA_4D_Principal");
 
     const action1 = mixer1.clipAction(clip);
     const action2 = mixer2.clipAction(clip);
@@ -80,7 +73,47 @@ phoenixLoader.load("./assests/phoenix_bird/scene.gltf",(gltf)=>{
     action3.play();
     action3.timeScale =0.5;
     action3.startAt(0.35);
-    window.addEventListener( "mouseover" , cameraAnimation);
+
+    dovLoader.load("./assests/dove_bird_rigged/scene.gltf",(gltf)=>{
+        const model = gltf.scene;
+        scene.add(model);
+        model.scale.set(0.2,0.2,0.2);
+        model.position.set(15,-40,0);
+        model.rotation.y = Math.PI/2
+        mixer4 = new THREE.AnimationMixer(model);
+        const  clips = gltf.animations;
+        const clip = THREE.AnimationClip.findByName(clips,"Take 001");
+        const action = mixer4.clipAction(clip);
+        action.play();
+        action.startAt(0.40);
+        // window.addEventListener( "mouseover" , cameraAnimation);
+    
+    });
+    phoenixLoader.load("./assests/realistic_jungle_tree_avatar/scene.gltf",(gltf)=>{
+        const model = gltf.scene;
+                    model.position.set(0,-60,-150)
+                    scene.add(model);
+        const model22 = SkeletonUtils.clone(model)
+        const model2 = SkeletonUtils.clone(model)
+        const model3 = SkeletonUtils.clone(model)
+        const model4 = SkeletonUtils.clone(model)
+        
+        scene.add(model4)
+        scene.add(model3)
+        scene.add(model2)
+        scene.add(model22)
+        model22.position.set(30,-50,-150)
+        model2.position.set(30,-90,-150)
+        model3.position.set(10,-90,-150)
+        model4.position.set(60,-90,-200)
+        
+        
+        
+
+    })
+
+  
+    window.addEventListener( "mousedown" , cameraAnimation);
         
 
 });
